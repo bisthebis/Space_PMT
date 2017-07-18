3d5d752356cb63683a7b679d8c3dfab85ded8cc5
bisthebis <bism25@hotmail.com>
2017-07-18 19:30:25 +0200

    Separated sub libraries

--- main.cpp
+++ main.cpp
@@ -21,8 +21,8 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
-#include "lib/console_logger.h"
-#include "lib/myexception.h"
+#include "utils/console_logger.h"
+#include "utils/myexception.h"
 #include <memory>
 
 
